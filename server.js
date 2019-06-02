2cfee0a559b995865bbf58bda82e24174af86bd0
Kayode Okunlade <okunladekayode@gmail.com>
2019-06-02 12:15:22 +0100

    fix heroku build

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'bundle.js'));
 });
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile('index.html');
 });
 app.listen(port);
