cfa3fafff773e6284655730a181ebaacf06c6d14
Kayode Okunlade <okunladekayode@gmail.com>
2019-06-02 12:15:22 +0100

    fix heroku build

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/ping', (req, res) => res.send('pong'));
 app.get('/bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'bundle.js'));
 });
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(port);
